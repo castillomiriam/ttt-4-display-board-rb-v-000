135daa560de7ad376c1aa635db94d68118b9e18e
Miriam Castillo <castillo.m.mc@gmail.com>
2018-01-30 03:34:28 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,3 @@
-# Define display_board that accepts a board and prints
-# out the current state.
 def display_board
  puts "   |   |   "
  puts "-----------"
@@ -7,5 +5,5 @@ def display_board
  puts "-----------"
  puts "   |   |   "
  end
- 
+
  display_board
