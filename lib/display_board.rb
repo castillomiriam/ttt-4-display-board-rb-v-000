0fd4f0528194595a5f508ca50513e5dd3f5b9978
Miriam Castillo <castillo.m.mc@gmail.com>
2018-01-30 03:49:34 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
-def display_board(board)
+  def display_board(board)
+ puts "   |   |   "
+ puts "-----------"
+ puts "   |   |   "
+ puts "-----------"
+ puts "   |   |   "
+ end
+ 
+display_board
 end
