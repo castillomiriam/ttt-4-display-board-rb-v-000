65cd23d097dc6e1056f3583aab2a04efb7c113cd
Miriam Castillo <castillo.m.mc@gmail.com>
2018-01-30 04:15:59 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,8 @@
   def display_board(board)
- puts "   |   |   "
+ puts " #{row[0]} |   |   "
  puts "-----------"
  puts "   |   |   "
  puts "-----------"
  puts "   |   |   "
- puts= "#{rows[0]}, #{rows[1]}, #{rows[2]}, #{rows[3]}, #{rows[4]},"
  end
 display_board
